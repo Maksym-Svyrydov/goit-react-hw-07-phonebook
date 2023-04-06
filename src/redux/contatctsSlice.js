f8e9c87a1ed5d4a8051df63a572536527fffbf69
Maksym-Svyrydov <110718671+Maksym-Svyrydov@users.noreply.github.com>
2023-04-06 16:25:42 +0300

    delete addMatcher

--- src/redux/contatctsSlice.js
+++ src/redux/contatctsSlice.js
@@ -1,4 +1,5 @@
-import { createSlice, isAnyOf } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
+// import { isAnyOf } from '@reduxjs/toolkit';
 import { fetchContacts, addContacts, deleteContacts } from './operation';
 
 const handlePending = state => {
@@ -19,45 +20,68 @@ export const contactsSlise = createSlice({
 
   extraReducers: builder =>
     builder
+      .addCase(fetchContacts.pending, handlePending)
       .addCase(fetchContacts.fulfilled, (state, { payload }) => {
+        state.isLoading = false;
+        state.error = null;
         state.items = payload;
       })
-
+      .addCase(fetchContacts.rejected, handleRejected)
+      .addCase(addContacts.pending, handlePending)
       .addCase(addContacts.fulfilled, (state, { payload }) => {
+        state.isLoading = false;
+        state.error = null;
         state.items.push(payload);
       })
+      .addCase(addContacts.rejected, handleRejected)
+      .addCase(deleteContacts.pending, handlePending)
       .addCase(deleteContacts.fulfilled, (state, { payload }) => {
+        state.isLoading = false;
+        state.error = null;
         const index = state.items.filter(contact => contact.id !== payload);
         state.items.splice(index, 1);
       })
+      .addCase(deleteContacts.rejected, handleRejected),
 
-      .addMatcher(
-        isAnyOf(
-          fetchContacts.pending,
-          addContacts.pending,
-          deleteContacts.pending
-        ),
-        handlePending
-      )
-      .addMatcher(
-        isAnyOf(
-          fetchContacts.rejected,
-          addContacts.rejected,
-          deleteContacts.rejected
-        ),
-        handleRejected
-      )
-      .addMatcher(
-        isAnyOf(
-          fetchContacts.fulfilled,
-          addContacts.fulfilled,
-          deleteContacts.fulfilled
-        ),
-        state => {
-          state.isLoading = false;
-          state.error = null;
-        }
-      ),
+  //  ninjas style:)
+  //     .addCase(fetchContacts.fulfilled, (state, { payload }) => {
+  //       state.items = payload;
+  //     })
+
+  //     .addCase(addContacts.fulfilled, (state, { payload }) => {
+  //       state.items.push(payload);
+  //     })
+  //     .addCase(deleteContacts.fulfilled, (state, { payload }) => {
+  //       const index = state.items.filter(contact => contact.id !== payload);
+  //       state.items.splice(index, 1);
+  //     })
+  //     .addMatcher(
+  //       isAnyOf(
+  //         fetchContacts.pending,
+  //         addContacts.pending,
+  //         deleteContacts.pending
+  //       ),
+  //       handlePending
+  //     )
+  //     .addMatcher(
+  //       isAnyOf(
+  //         fetchContacts.rejected,
+  //         addContacts.rejected,
+  //         deleteContacts.rejected
+  //       ),
+  //       handleRejected
+  //     )
+  //     .addMatcher(
+  //       isAnyOf(
+  //         fetchContacts.fulfilled,
+  //         addContacts.fulfilled,
+  //         deleteContacts.fulfilled
+  //       ),
+  //       state => {
+  //         state.isLoading = false;
+  //         state.error = null;
+  //       }
+  //     ),
 });
 
 export const contactsReducer = contactsSlise.reducer;
